130ab3cf862bb127f7c74b07331d5a585b72f60b
Nikolas Santis <nikosantis@gmail.com>
2019-11-24 20:19:28 -0300

    Testing in Frontend

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const { makeExecutableSchema } = require('graphql-tools')
 const express = require('express')
+const cors = require('cors')
 const gqlMiddleware = require('express-graphql')
 const { readFileSync } = require('fs')
 const { join } = require('path')
@@ -9,6 +10,7 @@ const resolvers = require('./lib/resolvers')
 
 const app = express()
 const port = process.env.port || 3000
+const isDev = process.env.NODE_ENV !== 'production'
 
 // definiendo el schema
 const typeDefs = readFileSync(
@@ -17,10 +19,12 @@ const typeDefs = readFileSync(
 )
 const schema = makeExecutableSchema({ typeDefs, resolvers })
 
+app.use(cors())
+
 app.use('/api', gqlMiddleware({
   schema: schema,
   rootValue: resolvers,
-  graphiql: true
+  graphiql: isDev
 }))
 
 app.listen(port, () => {
